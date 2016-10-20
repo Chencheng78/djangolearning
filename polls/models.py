05cb0b4e716ce829a956f7eeb06b0858a6776b28
Chencheng78 <geniuscc7@163.com>
2016-10-20 15:06:49 +0800

    django example learning #updated Tutorial7

--- polls/models.py
+++ polls/models.py
@@ -8,7 +8,7 @@ class Question(models.Model):
 	pub_date = models.DateTimeField('date published')
 	def was_published_recently(self):
 		now = timezone.now()
-		return now - datetime.timedelta(days=1) <= self.pub_date <= now
+		return now - datetime.timedelta(days=10) <= self.pub_date <= now
 	was_published_recently.admin_order_field = 'pub_date'
 	was_published_recently.boolean = True
 	was_published_recently.short_description = 'Published recently?'
